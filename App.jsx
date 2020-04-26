49055fc755cacd7d2d0fe76627b642c98ebd9f16
Dac-Trung Nguyen <dactrung.nguyen@gmail.com>
2020-04-26 17:52:04 +0200

    Component lifecycle

--- App.jsx
+++ App.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-   constructor() {
-      super();
-      this.state = {data: []};
-      this.setStateHandler = this.setStateHandler.bind(this);
-      this.forceUpdateHandler = this.forceUpdateHandler.bind(this);
-      this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+class Content extends React.Component {
+   componentWillMount() {
+      console.log('Component will mount')
    }
-
-   setStateHandler() {
-      var item = 'setState... '
-      var myArray = this.state.data.slice();
-      myArray.push(item);
-      this.setState({data: myArray});
+   componentDidMount() {
+      console.log('Component DID MOUNT!')
    }
-
-   forceUpdateHandler() {
-      this.forceUpdate();
+   componentWillReceiveProps(newProps) {    
+      console.log('Component WILL RECIEVE PROPS!')
    }
-
-   findDomNodeHandler() {
-      var myDiv = document.getElementById('myDiv');
-      ReactDOM.findDOMNode(myDiv).style.color = 'green';
+   shouldComponentUpdate(newProps, newState) {
+      return true;
    }
-
+   componentWillUpdate(nextProps, nextState) {
+      console.log('Component WILL UPDATE!')
+   }
+   componentDidUpdate(prevProps, prevState) {
+      console.log('Component DID UPDATE!')
+   }
+   componentWillUnmount() {
+      console.log('Component WILL UNMOUNT!')
+   }
    render() {
       return (
          <div>
-            <button onClick={this.setStateHandler}>Set state</button>
-            <h4>State array: {this.state.data}</h4>
-            <button onClick={this.forceUpdateHandler}>Force Update</button>
-            <h4>Random number: {Math.random()}</h4>
-            <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
-            <div id='myDiv'>NODE</div>
+            <h3>{this.props.myNumber}</h3>
+         </div>
+      )
+   }
+}
+
+class App extends React.Component {
+   constructor() {
+      super();
+      this.state = {data: 0}
+      this.setNewNumber = this.setNewNumber.bind(this);
+   }
+   setNewNumber() {
+      this.setState({data: this.state.data + 1});
+   }
+   render() {
+      return (
+         <div>
+            <button onClick={this.setNewNumber}>Increment</button>
+            <Content myNumber={this.state.data}></Content>
          </div>
       );
    }
